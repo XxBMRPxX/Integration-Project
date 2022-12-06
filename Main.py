8e6801c34509cd989978729465ac56e661104ca9
Michael Hierrezuelo <105249341+XxBMRPxX@users.noreply.github.com>
2022-12-06 09:19:11 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,94 +1,162 @@
-# Michael Hierrezuelo
 # Auto Loan Calculator
+# __author__ == Michael Hierrezuelo
 
-# Definitions
-# IN PROGRESS...
-def yes_no_confirmation(question):
-    yes = ["Yes", "Y", "yes", "y"]
-    no = ["No", "N", "no", "n"]
-    while question in no:
-        print("Please Try Again...")
-        firstName = input("Enter First Name: ")
-        lastName = input("Enter Last Name: ")
-        fullName = firstName + " " + lastName
-        print(fullName)
+def main():
+    # Definitions
+    def name_confirmation():
+        """
+        Confirm Inputted Name
+        :return: Yes/No
+        """
+        yes = ["Yes", "Y", "yes", "y"]
+        no = ["No", "N", "no", "n"]
         question = input("Is This Correct? Y or N? ")
-        yes_no_confirmation(question)
         if question in yes:
             print("Thank You!")
             return True
+        if question in no:
+            print("Please Try Again...")
+            firstName = input("Enter First Name: ")
+            lastName = input("Enter Last Name: ")
+            fullName = firstName + " " + lastName
+            print(fullName)
+            name_confirmation()
         else:
             print("Please Use 'Y' Or 'N'")
-            question = input("Is This Correct? Y or N? ")
-            yes_no_confirmation(question)
+            name_confirmation()
 
 
-# Introduction
-print("Welcome To Michael's Auto Loan Calculator!")
-print("Please Follow Next Steps And Input The Following To Get Started: ")
+    def birthday_confirmation():
+        """
+        Confirm Inputted Birthday
+        :return: Yes/No
+        """
+        yes = ["Yes", "Y", "yes", "y"]
+        no = ["No", "N", "no", "n"]
+        question = input("Is This Correct? Y or N? ")
+        if question in yes:
+            print("Thank You!")
+            return True
+        if question in no:
+            print("Please Try Again...")
+            birthMonth = input("Enter Birth Month: ")
+            birthDay = input("Enter Birth Day: ")
+            birthYear = input("Enter Birth Year: ")
+            fullBirthday = (birthMonth + "/" + birthDay + "/" + birthYear)
+            print(fullBirthday)
+            birthday_confirmation()
+        else:
+            print("Please Use 'Y' Or 'N'")
+            birthday_confirmation()
 
-# Name Entry
-print("Let's Begin With Your Name!")
-firstName = input("Enter First Name: ")
-lastName = input("Enter Last Name: ")
-fullName = firstName + " " + lastName
-print(fullName)
 
-# Name Confirmation
-question = input("Is This Correct? Y or N? ")
-yes_no_confirmation(question)
+    def loan_data_confirmation():
+        """
+        Confirm Inputted Loan Data
+        :return: Yes/No
+        """
+        yes = ["Yes", "Y", "yes", "y"]
+        no = ["No", "N", "no", "n"]
+        question = input("Is This Correct? Y or N? ")
+        if question in yes:
+            print("Thank You!")
+            return True
+        if question in no:
+            print("Please Try Again...")
+            loanAmount = int(input("Please Enter The Amount On The Loan: $"))
+            downPayment = int(input("Down Payment?: $"))
+            loanAPR = float(input("Enter Loan Annual Percentage Rate (APR): "))
+            loanTerm = int(input("Enter Loan Term (In Months): "))
+            taxPercentage = float(input("Enter State Tax Percentage: "))
+            print("Loan Amount: $", loanAmount, sep="")
+            print("Down Payment: $", downPayment, sep="")
+            print("Loan Term:", loanTerm, "Month(s)")
+            print("Annual Percentage Rate: ", loanAPR, "%", sep="")
+            print("State Tax Percentage: ", taxPercentage, "%", sep="")
+            loan_data_confirmation()
+        else:
+            print("Please Use 'Y' Or 'N'")
+            loan_data_confirmation()
 
-# Birthday Entry
-print("Now Let's Confirm Your Date Of Birth")
-print("Please Enter Your Birthday Starting With MM")
-birthMonth = input("Enter Birth Month: ")
-birthDay = input("Enter Birth Day: ")
-birthYear = input("Enter Birth Year: ")
-fullBirthday = (birthMonth + "/" + birthDay + "/" + birthYear)
-print(fullBirthday)
-print("Please Type Yes/Y Or No/N")
 
-# Birthday Confirmation
-dateOfBirthConfirmation = input("Is This Correct? ")
-if dateOfBirthConfirmation.upper() == "Y" or dateOfBirthConfirmation.upper() == "Yes":
-    print("Thank You!")
-elif dateOfBirthConfirmation.upper() == "N" or dateOfBirthConfirmation.upper() == "No":
-    print("Return Back To Line 22")
+    def reset_calculator():
+        """
+        Reset The Calculator Program
+        :return: Yes/No
+        """
+        yes = ["Yes", "Y", "yes", "y"]
+        no = ["No", "N", "no", "n"]
+        question = input("Reset Calculator? ")
+        if question in yes:
+            print("Resetting...")
+            main()
+        if question in no:
+            print("Thank You For Using Michael's Auto Loan Calculator! :)")
+        else:
+            print("Please Use 'Y' Or 'N'")
+            reset_calculator()
 
-# Loan Details Entry
-loanAmount = input("Enter Loan Amount: $")
-downPayment = input("Down Payment: $")
-loanAPR = input("Enter Loan Annual Percentage Rate (APR): ")
-loanTerm = input("Enter Loan Term (In Months): ")
-taxPercentage = input("Enter Tax Percentage: ")
-print("Please Review: Your Loan Amount Of $" + loanAmount, "With A Down Payment Of $" + downPayment, "For A Term Of ",
-      loanTerm, "Months At An Annual Percentage Rate Of", loanAPR + "% And The Tax Percentage Of", taxPercentage + "%")
-print("Please Type Yes/Y Or No/N")
 
-# Loan Details Confirmation
-loanDataInformation = input("Is This Correct? ")
-if loanDataInformation.upper() == "Y" or loanDataInformation.upper() == "Yes":
-    print("Thank You!")
-elif loanDataInformation.upper() == "N" or loanDataInformation.upper() == "No":
-    print("Return Back To Line 38")
+    # Introduction
+    print("Welcome To Michael's Auto Loan Calculator!")
+    print("Please Follow Next Steps And Input The Following To Get Started: ")
 
-# In Progress...
-# print("Calculating...")
-# afterDownPayment = loanAmount - downPayment
-# annualInterestRate = float(loanAPR / 100)
-# totalWithAPR = afterDownPayment * (1 +annualInterestRate)
-# monthlyTotalBeforeTax = annualInterestAmount / 12
-# taxRate = float(taxPercentage / 100)
-# monthlyTotalAfterTax = monthlyTotalBeforeTax * (1 + taxRate)
-print("Done!")
-# print("Your Monthly Payment: ", monthlyTotalAfterTax)
-print("Would You Like To Edit/Make Changes?")
-restartCalculator = input("Yes or No? ")
-if restartCalculator.upper() == "Y" or restartCalculator.upper() == "Yes":
-    print("Thank You!")
-elif restartCalculator.upper() == "N" or restartCalculator.upper() == "No":
-    print("Return Back To Line 7")
-# In Progress...
+    # Name Entry
+    print("Let's Begin With Your Name!")
+    firstName = input("Enter First Name: ")
+    lastName = input("Enter Last Name: ")
+    fullName = firstName + " " + lastName
+    print(fullName)
+
+    # Name Confirmation
+    name_confirmation()
+
+    # Birthday Entry
+    print("Now Let's Confirm Your Date Of Birth")
+    print("Please Enter Your Birthday Using Digits (MM/DD/YYYY)")
+    birthMonth = input("Enter Birth Month: ")
+    birthDay = input("Enter Birth Day: ")
+    birthYear = input("Enter Birth Year: ")
+    fullBirthday = (birthMonth + "/" + birthDay + "/" + birthYear)
+    print(fullBirthday)
+
+    # Birthday Confirmation
+    birthday_confirmation()
+
+    # Loan Details Entry
+    print("Now Let's Get Started...")
+    loanAmount = int(input("Please Enter The Amount On The Loan: $"))
+    downPayment = int(input("Down Payment?: $"))
+    loanAPR = float(input("Enter Loan Annual Percentage Rate (APR): "))
+    loanTerm = int(input("Enter Loan Term (In Months): "))
+    taxPercentage = float(input("Enter State Tax Percentage: "))
+    print("Loan Amount: $", loanAmount, sep="")
+    print("Down Payment: $", downPayment, sep="")
+    print("Loan Term:", loanTerm, "Month(s)")
+    print("Annual Percentage Rate: ", loanAPR, "%", sep="")
+    print("State Tax Percentage: ", taxPercentage, "%", sep="")
+
+    # Loan Details Confirmation
+    loan_data_confirmation()
+
+    print("Calculating...")
+
+    afterDownPayment = loanAmount - downPayment
+    taxRateDecimal = float(taxPercentage / 100)
+    annualTotalAfterTax = afterDownPayment * (1.0 + taxRateDecimal)
+    monthlyTotalAfterTax = annualTotalAfterTax / 12
+
+    annualInterestRateDecimal = float(loanAPR / 100)
+    annualTotalWithAPR = annualTotalAfterTax * (1.0 + annualInterestRateDecimal)
+    monthlyTotalWithAPR = annualTotalWithAPR / 12
+    print("Done!")
+    print("Your Monthly Payment: ", monthlyTotalWithAPR)
+
+    # Restart Calculator?
+    reset_calculator()
+
+
+main()
 
 # Sprint 1 Additional Requirements:
 
@@ -106,7 +174,9 @@ elif restartCalculator.upper() == "N" or restartCalculator.upper() == "No":
 # print("Hello" * 5) = HelloHelloHelloHelloHello (Types "Hello" 5 Times Together)
 # print ("Hello"+"Michael") = HelloMichael (Brings The Two Strings Together)
 
+
 # Sprint 2 Additional Requirements:
+
 # Shortcut Operators: x += 1 is the same (==) as x = x+ 1
 # x == 1 means that the x must actually equal the same value as 1
 # x != 2 means x must not actually equal to one
@@ -114,4 +184,8 @@ elif restartCalculator.upper() == "N" or restartCalculator.upper() == "No":
 # Boolean: "and" is a "conditional" word used to make sure it goes for both "this" condition and "that" condition
 # Boolean: "not" is also a conditional word used to make sure it is "not" meeting that specific condition(s)
 # "While" is a Loop word that continues over and over usually for unknown amount of responses in the input
+#for, in, range
 #
+#for i in range(1,101,2):
+#    print(x)
+
